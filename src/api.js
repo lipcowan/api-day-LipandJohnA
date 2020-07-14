1dfaa813ec91c8a867e1387ad817280c64bd06a9
Lip Cowan <pcowan90@gmail.com>
2020-07-14 09:27:50 -0700

    exercises 4 and 5

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export default {getItems};
+export default {getItems, createItem};
 
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/lip';
 
@@ -6,3 +6,13 @@ function getItems() {
     return fetch(`${BASE_URL}/items`);
 }
 
+function createItem(name) {
+    let newItem = {name};
+    
+    return fetch(`${BASE_URL}/items`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(newItem)
+    }
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import $ from 'jquery';
-
 import 'normalize.css';
 import './index.css';
-
 import shoppingList from './shopping-list';
-
-import api from './api'
+import api from './api';
+import store from './store';
 
 const main = function () {
   api.getItems()
   .then(res => res.json())
-  .then(res => console.log(res));
+  .then((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render(); // not calling the method, passing it 
+  });
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
